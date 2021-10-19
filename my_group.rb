dbd5f4e818258c4f457ddda02c29ee350fc10b5d
giacoletti <giacoletti92@gmail.com>
2021-10-19 10:17:02 +0200

    creating my_group file and adding persons data

---
+++ my_group.rb
@@ -0,0 +1,10 @@
+#Create an array and store it in a variable called my_group
+my_group = []
+
+#Create three hashes describing an individual and name them person_1, person_2 and person_3. 
+person_1 = { name: "Luca", gender: "male", age: "26"}
+person_2 = { name: "Francesco", gender: "male", age: "32"}
+person_3 = { name: "Gaia", gender: "female", age: "24"}
+
+#Add all people to your my_group array.
+my_group = [person_1, person_2, person_3]
