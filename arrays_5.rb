db17fafec82d1e78c75916f28f695f29e3b204eb
giacoletti <giacoletti92@gmail.com>
2021-10-20 20:58:38 +0200

    accessing a string using indexes

---
+++ arrays_5.rb
@@ -0,0 +1,8 @@
+string = "Welcome to America!"
+a = string[6]   
+b = string[11]
+c = string[19]
+#string are arrays of characters. "a" is the seventh character of the string, last 'e' of 
+#the word "Welcome".
+#"b" is the 12th character of the string, 'A'
+#"c" is "nil", empty, undefined. There is no 20th character in the string.
