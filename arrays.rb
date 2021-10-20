d3cdf2b2ffd2a567ac278eef7cad4f82b3588b0c
giacoletti <giacoletti92@gmail.com>
2021-10-20 19:29:43 +0200

    small program to check if 3 is inside an array

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+    puts "The number #{number} is contained in the array"
+else
+    puts "The number #{number} is not contained in the array"
+end
