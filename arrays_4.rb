b375e38e8ffd8cc49e8fdbd00c6da4cfde3ae452
giacoletti <giacoletti92@gmail.com>
2021-10-20 20:44:44 +0200

    .index method and accessing an array

---
+++ arrays_4.rb
@@ -0,0 +1,7 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+arr.index(5) #it returns the index of the number 5 inside the array, which is 3.
+
+arr.index[5] #it returns an error. You can't use square brackets with the .index method
+
+arr[5] #it returns 8, the fifth object of the array.
