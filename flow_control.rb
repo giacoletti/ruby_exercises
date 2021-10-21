4805f72786c467f9325a05f5ae505a99dd647548
giacoletti <giacoletti92@gmail.com>
2021-10-21 20:04:15 +0200

    true or false expressions analysis

---
+++ flow_control.rb
@@ -0,0 +1,9 @@
+(32 * 4) >= 129  #returns false 
+
+false != !true #returns false 
+
+true == 4 #returns false
+
+false == (847 == '847') #returns true
+
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false  #returns true
