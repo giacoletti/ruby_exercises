992b0d4c12c851d0bf797a8d97039844037de114
giacoletti <giacoletti92@gmail.com>
2021-10-22 09:50:23 +0200

    concatenating 2 strings inside a method

---
+++ methods_4.rb
@@ -0,0 +1,7 @@
+def scream(words)
+    words = words + "!!!!"
+    # return          #this return leaves the method without printing anything
+    puts words
+end
+
+scream("Yippeee") # => Yippeee!!!!
